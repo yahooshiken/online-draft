1c54811bf095df6e7ec5c02a34d424734a9c5a3d
Hoshi <kenya.hoshimure@pol.co.jp>
2020-05-04 11:21:52 +0900

    :sparkles: launch express server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,16 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+import express from "express";
 
-console.log(hello("World!!"));
+const PORT = process.env.PORT || 1844;
+
+const app: express.Express = express();
+const router: express.Router = express.Router();
+
+router.get("*", (req: express.Request, res: express.Response) => {
+  res.send("Get");
+});
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}!`);
+});
