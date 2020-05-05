17bf2a51cbcbd79be512147eb78f2165f1d534c3
Hoshi <hoshimure.47@gmail.com>
2020-05-06 03:05:35 +0900

    :art: arrange before start page.

--- client/src/domains/user_list/components/UserList/UserList.tsx
+++ client/src/domains/user_list/components/UserList/UserList.tsx
@@ -1,19 +1,19 @@
 import React, { FC } from "react";
+import { Box, Text } from "rebass";
 import { UserModel } from "../../user_list_reducers";
 
 type Props = {
   userList: Array<UserModel>;
 };
 
-const UserList: FC<Props> = ({ userList = [] }) => {
-  console.info(userList);
-  return (
-    <div>
-      {userList.map((user) => (
-        <li key={user._id}>{user.name}</li>
-      ))}
-    </div>
-  );
-};
+const UserList: FC<Props> = ({ userList = [] }) => (
+  <Box>
+    {userList.map((user, index) => (
+      <Box key={user._id} p={16} sx={{ borderBottom: "solid 1px #aaaaaa" }}>
+        <Text fontWeight="bold">{`${index + 1}．${user.name}`}</Text>
+      </Box>
+    ))}
+  </Box>
+);
 
 export default UserList;
